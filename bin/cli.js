9e68fb98efccbf428066e4f59f99efe8877c4177
linchengzhe <linchengzhe@wandu.cn>
2021-10-27 23:06:51 +0800

    feat:接收用户传入参数配置

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,16 @@
-// 启动文件
+#! /usr/bin/env node
+const program = require('commander')
+
+program
+.command('create <app-name>')
+.description('Create a new project')
+.option('-f --force','overwrite target directory if it exist')
+.action((name,options) => {
+    console.log('name',name,'option',options)
+})
+
+program
+.version(`v${require('../package.json').version}`)
+.usage('<command> [options]')
+
+program.parse(process.argv)
