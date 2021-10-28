24dbc868578f03a2bfb727826ccfb90d0c233d29
linchengzhe <linchengzhe@wandu.cn>
2021-10-28 22:01:06 +0800

    feat:引入axios获取远程模板信息

--- bin/create.js
+++ bin/create.js
@@ -2,6 +2,8 @@
 const path = require("path");
 const fs = require("fs-extra");
 const inquire = require("inquirer");
+const Generator = require("../lib/Generator");
+
 module.exports = async (name, option) => {
   // 当前命令行所在目录
   const cwd = process.cwd();
@@ -19,17 +21,22 @@ module.exports = async (name, option) => {
           type: "list",
           message: "目标路径已有文件,是否选择覆盖",
           choices: [
-            { name: "Ok", value: "true" },
-            { name: "cancel", value: "fasle" },
+            { name: "确认", value: "true" },
+            { name: "取消", value: "fasle" },
           ],
         },
       ]);
       if (!action) {
         return;
-      }else if(action === 'true'){
-        console.log("覆盖文件~")
-        await fs.remove(targetDir)
+      } else if (action === "true") {
+        console.log("覆盖文件~");
+        await fs.remove(targetDir);
       }
     }
   }
+
+  // 创建项目
+  const generator = new Generator(name, targetDir);
+  // 执行
+  generator.create();
 };
